db3995d05534ba4288e2d85bd7f4250217edbe90
Van Nguyen <93vannd@gmail.com>
2020-06-15 10:40:38 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { theme } from "./Theme";
+
+import Login from "./pages/Login";
+import AppLayout from "./layouts/AppLayout";
+
+import Calendar from "./pages/Calendar";
+import Dishes from "./pages/Dishes";
+import ShoppingList from "./pages/ShoppingList";
+import History from "./pages/History";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <AppLayout>
+              <Route path="/calendar" component={Calendar} />
+              <Route path="/dishes" component={Dishes} />
+              <Route path="/shopping-list" component={ShoppingList} />
+              <Route path="/history" component={History} />
+            </AppLayout>
+          </Switch>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
